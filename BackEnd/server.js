9942607ec22461e4a43bd544b3e19caa16ec2f91
lfcpacman <cammarol@hochschule-trier.de>
2020-05-15 22:20:24 +0200

    debug script working

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,14 +1,11 @@
-const express = require("express");
-let server = express();
+const express = require('express');
+const server = express();
 let port = 8080;
-if ((process.argv[2])&&(process.argv[2]>0))
-{
-    port = process.argv[2];
+if ((process.argv[2]) && (process.argv[2] > 0)) {
+  port = process.argv[2];
 }
-server.listen(port,function(){
-    console.log('server started on port: ' + port);
-  });
+server.listen(port, function () {
+  console.log('server started on port: ' + port);
+});
 
-server.use(express.static("../FrontEnd/Dist"));
-
-
+server.use(express.static('../FrontEnd/Dist'));
