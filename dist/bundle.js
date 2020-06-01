6f52c432570a316143739fe755c149c265eb0a2b
Kevin2751992 <ke-messmer@gmx.de>
2020-06-01 21:00:07 +0200

    nodemon config changed, basic header added

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,19 @@
-!function u(i,c,d){function f(t,e){if(!c[t]){if(!i[t]){var n="function"==typeof require&&require;if(!e&&n)return n(t,!0);if(l)return l(t,!0);var o=new Error("Cannot find module '"+t+"'");throw o.code="MODULE_NOT_FOUND",o}var r=c[t]={exports:{}};i[t][0].call(r.exports,function(e){return f(i[t][1][e]||e)},r,r.exports,u,i,c,d)}return c[t].exports}for(var l="function"==typeof require&&require,e=0;e<d.length;e++)f(d[e]);return f}({1:[function(e,t,n){function o(){console.log("clicked"),document.getElementById("testButton").innerHTML="YOU CLICKED ME!"}e("./test"),document.addEventListener("DOMContentLoaded",function(e){console.log("loaded"),document.getElementById("testButton").addEventListener("click",o)})},{"./test":2}],2:[function(e,t,n){console.log("test 2")},{}]},{},[1]);
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+require('./test');
+
+// Add Eventlistener here
+document.addEventListener('DOMContentLoaded', function (event) {
+  // the event occurred
+  console.log('loaded');
+  document.getElementById('testButton').addEventListener('click', myFunction);
+});
+
+function myFunction () {
+  console.log('clicked');
+  document.getElementById('testButton').innerHTML = 'YOU CLICKED ME!';
+}
+
+},{"./test":2}],2:[function(require,module,exports){
+console.log('test 2');
+
+},{}]},{},[1]);
