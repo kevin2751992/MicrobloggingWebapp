4f2615f9de246b8bbb3f4b88826fc3f4f8596ad7
Kevin2751992 <ke-messmer@gmx.de>
2020-06-21 23:49:07 +0200

    pagination (without logic) added

--- src/client/js/pagination.js
+++ src/client/js/pagination.js
@@ -3,9 +3,34 @@ export class Pagination {
   constructor (numberOfBlogPosts) {
     console.log('Amount of BlogPosts: ', numberOfBlogPosts);
     this.numberOfBlogPosts = numberOfBlogPosts;
+    this.index = 0;
   }
 
   createPagination () {
+    // get max Page and round it up
+    const maxPageNumber = Math.ceil(this.numberOfBlogPosts / 10);
+    const paginationContainer = document.getElementById('paginationContainer');
+    const indexContainer = document.createElement('div');
+    indexContainer.className = 'indexContainer';
 
+    // add here pagination for 100+ blogentries
+    for (let i = 1; i <= maxPageNumber; i++) {
+      const paginationlink = document.createElement('a');
+      paginationlink.innerHTML = i;
+      if (i === 1) {
+        console.log('active');
+        paginationlink.className = 'active';
+      }
+      indexContainer.appendChild(paginationlink);
+    }
+    paginationContainer.appendChild(indexContainer);
+  }
+
+  nextPage () {
+    this.index++;
+  }
+
+  prevPage () {
+    this.index--;
   }
 }
