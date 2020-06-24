d2e3ea5f4364877721fc560f0df12b70663cb64e
Kevin2751992 <ke-messmer@gmx.de>
2020-06-25 00:36:56 +0200

    Modal Handler added

--- src/client/components/Modal.js
+++ src/client/components/Modal.js
@@ -4,6 +4,32 @@ export class Modal {
     const modal = document.getElementById('modal');
     modal.classList.add('show');
 
+    document.getElementById('closeModal').addEventListener('click', () => {
+      console.log('close');
+      document.getElementById('modal').classList.remove('show');
+      document.getElementById('modal').classList.add('hide');
+    });
+
+    document.getElementById('post').addEventListener('click', () => {
+      console.log('submit');
+    });
+
+    document.getElementById('radioGeo').addEventListener('change', (event) => {
+      console.log('radio', event.target.value);
+      if (event.target.value === 'geo') {
+        document.getElementById('fileUpload').classList.remove('show');
+        document.getElementById('fileUpload').classList.add('hide');
+      }
+    });
+    document.getElementById('radioImg').addEventListener('change', (event) => {
+      console.log('changed');
+      if (event.target.value === 'img') {
+        console.log('show img upload');
+        document.getElementById('fileUpload').classList.remove('hide');
+        document.getElementById('fileUpload').classList.add('show');
+      }
+    });
+
     // Use this later for Timestamp StackoverFlow is nice!
     var today = new Date();
     var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
