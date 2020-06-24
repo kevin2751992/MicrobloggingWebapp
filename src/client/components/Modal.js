82821cc9c87f47b2ddb630956ac4169937124c6e
Kevin2751992 <ke-messmer@gmx.de>
2020-06-24 21:06:39 +0200

    styling and fileupload input added (currently hidden)

--- src/client/components/Modal.js
+++ src/client/components/Modal.js
@@ -3,5 +3,12 @@ export class Modal {
     console.log('hi');
     const modal = document.getElementById('modal');
     modal.classList.add('show');
+
+    // Use this later for Timestamp StackoverFlow is nice!
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    var dateTime = date + ' ' + time;
+    console.log('Date:', dateTime);
   }
 }
