ac90bd9bc38165df4625cbb1204bc9200224d8ac
Kevin2751992 <ke-messmer@gmx.de>
2020-06-25 00:45:21 +0200

    bundle

--- src/client/components/Modal.js
+++ src/client/components/Modal.js
@@ -1,19 +1,27 @@
 export class Modal {
   constructor () {
     console.log('hi');
+    // set Modal on show
     const modal = document.getElementById('modal');
     modal.classList.add('show');
 
+    // -----CloseHandler----> Close Modal on Click
     document.getElementById('closeModal').addEventListener('click', () => {
       console.log('close');
       document.getElementById('modal').classList.remove('show');
       document.getElementById('modal').classList.add('hide');
     });
-
+    // -----Submit----> Create new BlogPost
     document.getElementById('post').addEventListener('click', () => {
       console.log('submit');
+      // Timestamp
+      var today = new Date();
+      var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+      var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+      var dateTime = date + ' ' + time;
+      console.log('Date:', dateTime);
     });
-
+    // ---RadioHandler Show and hide File updload
     document.getElementById('radioGeo').addEventListener('change', (event) => {
       console.log('radio', event.target.value);
       if (event.target.value === 'geo') {
@@ -29,12 +37,5 @@ export class Modal {
         document.getElementById('fileUpload').classList.add('show');
       }
     });
-
-    // Use this later for Timestamp StackoverFlow is nice!
-    var today = new Date();
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-    var dateTime = date + ' ' + time;
-    console.log('Date:', dateTime);
   }
 }
