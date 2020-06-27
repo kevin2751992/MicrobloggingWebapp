a9dc41298311bc3c49d8d1fcecc6ce734a0dbf1a
Kevin2751992 <ke-messmer@gmx.de>
2020-06-27 13:41:13 +0200

    Submit get data (next fetch post api)

--- src/client/components/Modal.js
+++ src/client/components/Modal.js
@@ -14,27 +14,60 @@ export class Modal {
     // -----Submit----> Create new BlogPost
     document.getElementById('post').addEventListener('click', () => {
       console.log('submit');
+      // MetaData
       // Timestamp
       var today = new Date();
       var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
       var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
       var dateTime = date + ' ' + time;
       console.log('Date:', dateTime);
+      // Author
+      const userName = document.getElementById('user').innerHTML;
+      console.log('User:', userName);
+      const userImg = document.getElementById('userImg').src;
+      console.log('UserImgUrl:', userImg);
+
+      // BlogPost
+      // Title
+      const title = document.getElementById('blogtitle').value;
+      console.log('Title:', title);
+      if (title === '') {
+        window.alert('Title cannot be left blanc');
+      }
+      // Content
+      const blogBody = document.getElementById('contentInput').value;
+      console.log('Body:', blogBody);
+      if (blogBody === '') {
+        window.alert('Body cannot be left blanc');
+      }
+
+      // Optional Media
+      const radioImg = document.getElementById('radioImg').checked;
+      console.log('ImgRadio is checked?', radioImg);
+      if (radioImg) {
+        console.log('IMG is checked');
+        const imgFile = document.getElementById('file').files[0];
+        // genarateID
+        const imgID = imgFile.name + Math.floor(Math.random() * 999999);
+        console.log('IMG', imgID);
+      }
+
+      // Error handling
     });
     // ---RadioHandler Show and hide File updload
     document.getElementById('radioGeo').addEventListener('change', (event) => {
       console.log('radio', event.target.value);
       if (event.target.value === 'geo') {
-        document.getElementById('fileUpload').classList.remove('show');
-        document.getElementById('fileUpload').classList.add('hide');
+        document.getElementById('imgInput').classList.remove('show');
+        document.getElementById('imgInput').classList.add('hide');
       }
     });
     document.getElementById('radioImg').addEventListener('change', (event) => {
       console.log('changed');
       if (event.target.value === 'img') {
         console.log('show img upload');
-        document.getElementById('fileUpload').classList.remove('hide');
-        document.getElementById('fileUpload').classList.add('show');
+        document.getElementById('imgInput').classList.remove('hide');
+        document.getElementById('imgInput').classList.add('show');
       }
     });
   }
